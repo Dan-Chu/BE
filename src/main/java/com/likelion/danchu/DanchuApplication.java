1a8932ff728a90f2ed6dd8d155c16db5e33fe481
김나경 <1030n@naver.com>
2025-08-01 19:15:00 +0900

    :tada: 단추 프로젝트 시작

---
+++ src/main/java/com/likelion/danchu/DanchuApplication.java
@@ -0,0 +1,12 @@
+package com.likelion.danchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanchuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DanchuApplication.class, args);
+  }
+}
---
+++ src/test/java/com/likelion/danchu/DanchuApplicationTests.java
@@ -0,0 +1,11 @@
+package com.likelion.danchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanchuApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}

33379fe01979150d4cece852d3d768de83fe9736
shinchaerin79 <shinchaerin79@gmail.com>
2025-08-04 03:43:09 +0900

    :sparkles: [FEAT] BaseTimeEntity 적용 및 JPA Auditing 설정 추가

--- src/main/java/com/likelion/danchu/DanchuApplication.java
+++ src/main/java/com/likelion/danchu/DanchuApplication.java
@@ -2,8 +2,10 @@ package com.likelion.danchu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DanchuApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/likelion/danchu/domain/hashtag/controller/HashtagController.java
+++ src/main/java/com/likelion/danchu/domain/hashtag/controller/HashtagController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.likelion.danchu.domain.hashtag.dto.request.HashtagRequest;
 import com.likelion.danchu.domain.hashtag.dto.response.HashtagResponse;
 import com.likelion.danchu.domain.hashtag.service.HashtagService;
+import com.likelion.danchu.global.response.BaseResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,9 +28,10 @@ public class HashtagController {
 
   @Operation(summary = "해시태그 등록", description = "새로운 해시태그를 추가할 때 사용하는 API입니다.")
   @PostMapping
-  public ResponseEntity<HashtagResponse> createHashtag(
+  public ResponseEntity<BaseResponse<HashtagResponse>> createHashtag(
       @Valid @RequestBody HashtagRequest hashtagRequest) {
     HashtagResponse hashtagResponse = hashtagService.createHashtag(hashtagRequest);
-    return ResponseEntity.status(HttpStatus.CREATED).body(hashtagResponse);
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(BaseResponse.success("해시태그 생성에 성공했습니다.", hashtagResponse));
   }
 }

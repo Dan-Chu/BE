43499fad25fde3c7f933cf42cc016415d11a5df6
shinchaerin79 <shinchaerin79@gmail.com>
2025-08-08 05:16:31 +0900

    :sparkles: Store와 Hashtag 중복 여부 확인 메서드 추가

--- src/main/java/com/likelion/danchu/domain/store/repository/StoreHashtagRepository.java
+++ src/main/java/com/likelion/danchu/domain/store/repository/StoreHashtagRepository.java
@@ -3,7 +3,12 @@ package com.likelion.danchu.domain.store.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.likelion.danchu.domain.hashtag.entity.Hashtag;
+import com.likelion.danchu.domain.store.entity.Store;
 import com.likelion.danchu.domain.store.entity.StoreHashtag;
 
 @Repository
-public interface StoreHashtagRepository extends JpaRepository<StoreHashtag, Long> {}
+public interface StoreHashtagRepository extends JpaRepository<StoreHashtag, Long> {
+
+  boolean existsByStoreAndHashtag(Store store, Hashtag hashtag);
+}
